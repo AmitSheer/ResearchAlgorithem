437b31554154889c68deef132733c7a194233034
AmitSheer <amitcohen95@gmail.com>
2022-03-06 19:36:47 +0200

    read module successfully

--- assignment_two/doc_to_html.py
+++ assignment_two/doc_to_html.py
@@ -7,7 +7,5 @@ out_script_name = sys.argv[2]
 
 module = importlib.import_module(in_script_name.removesuffix('.py'))
 print(module.__name__)
-print()
-# print(f'{in_script_name}')
-# print(f'{out_script_name}')
-# print()
+print(module.__doc__)
+print(module.__dir__()[8:])
