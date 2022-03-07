4f183cb3ec4b95dd6ccca7a2794ce4fb8b6501a0
Amit-Sheer Cohen <amitcohen95@gmail.com>
2022-03-07 17:38:53 +0200

    a

--- assignment_two/question3.py
+++ assignment_two/question3.py
@@ -3,12 +3,20 @@ class List(list):
         super().__init__(seq)
 
     def __getitem__(self, *args):
-        current = super().__getitem__(args[0][0])
-        for index in args[0][1:]:
-            current = current[index]
+        if len(args) > 1:
+            current = super().__getitem__(args[0][0])
+            for index in args[0][1:]:
+                current = current[index]
+        else:
+            current = super().__getitem__(args[0])
         return current
 
 
-new_list = List([[[1, 2, 3], [3, 9, 1]], [[4, 5, 6], [6, 4, 5]]])
-a = new_list[0, 1, 1]
-print(a)
+new_list = list([
+    [[1, 2, 3], [3, 9, 1]],
+    [[4, 5, 6], [6, 4, 5]]])
+k = List([1, 2, 3])
+# a = new_list[0, 1, 1]
+# a = new_list[0][1][1]
+# print(a)
+print(k[1])
