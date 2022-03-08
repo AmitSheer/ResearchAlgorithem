5945710b686ef392e9f5f0a7ad2b61849647691d
AmitSheer <amitcohen95@gmail.com>
2022-03-08 16:00:53 +0200

    added tests for 2 and 1

--- assignment_two/doc_to_html.py
+++ assignment_two/doc_to_html.py
@@ -1,11 +1,33 @@
 import importlib
 import sys
 
-sys.path.append('assignment_two')
-in_script_name: str = sys.argv[1]
-out_script_name = sys.argv[2]
 
-module = importlib.import_module(in_script_name.removesuffix('.py'))
-print(module.__name__)
-print(module.__doc__)
-print(module.__dir__()[8:])
+# sys.path.append('assignment_two')
+# in_script_name: str = sys.argv[1]
+# out_script_name = sys.argv[2]
+
+
+def to_html():
+    def getContent(loaded_module):
+        content = f"<h1>Module {loaded_module.__name__}</h1> {loaded_module.__doc__}"
+        for f in loaded_module.__dir__()[8:]:
+            func = loaded_module.__getattribute__(f)
+            content += f"<h1>Function {f}:</h1><wbr>{func.__doc__}<h3>Annotations:</h3>"
+            for param in func.__annotations__:
+                content += f"{param}<br>"
+        return content
+
+    module = importlib.import_module(in_script_name.removesuffix('.py'))
+    html = "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\"><html><head><meta " \
+           "http-equiv=\"Content-Type\" content=\"text/html; " \
+           "charset=utf-8\"><style></style></head><body><div>" + \
+           getContent(module) + "</div></body></html>"
+
+    with open(out_script_name, 'w') as file:
+        file.write(html)
+
+
+if __name__ == '__main__':
+    in_script_name = "module_example"
+    out_script_name = "py_to_html.html"
+    to_html()
--- assignment_two/test.py
+++ assignment_two/test.py
@@ -1,10 +1,29 @@
 import unittest
+import doc_to_html
+from question2 import lastcall
+
+
+@lastcall
+def kkk(num: int):
+    return num * 4
 
 
 class Tests(unittest.TestCase):
-    def test_decorative(self):
-        self.assertEqual(True, False)  # add assertion here
 
+    def test_check_doc_to_html(self):
+        doc_to_html.in_script_name = "module_example"
+        doc_to_html.out_script_name = "py_to_html_out.html"
+        doc_to_html.to_html()
+        with open(doc_to_html.out_script_name, 'r') as file:
+            output = file.read().replace(" ", "")
+        with open("expected.html", 'r') as file:
+            expected = file.read().replace(" ", "")
+        self.assertEqual(expected, output)
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_check_question2(self):
+        self.assertEqual(4, kkk(1))
+        self.assertEqual("value already calculated:4", kkk(1))
+        self.assertEqual(8, kkk(2))
+        self.assertEqual("value already calculated:4", kkk(1))
+        self.assertEqual("value already calculated:8", kkk(2))
+
