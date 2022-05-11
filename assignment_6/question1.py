02c801ec37b1a2c4f7f78cf14fc735dcf344a518
Amit-Sheer Cohen <amitcohen95@gmail.com>
2022-05-11 11:39:14 +0300

    finished q2

--- assignment_6/question1.py
+++ assignment_6/question1.py
@@ -9,7 +9,7 @@ def plotIntersection(space, f, g):
     point_set: set = set()
     for i in range(math.ceil(space.min()), math.ceil(space.max())):
         x = fsolve(lambda x: f(x) - g(x), i)[0]
-        if round(f(x), 3) == round(g(x), 3):
+        if round(f(x), 3) == round(g(x), 3) and space.min()<=x<=space.max():
             point_set.add(x)
     arr = np.array([[x, f(x)] for x in point_set])
     plt.grid()
@@ -24,10 +24,11 @@ if __name__ == '__main__':
     g1 = lambda x: x + 10
     f2 = lambda x: np.sin(x)
     g2 = lambda x: 0.2 * x
+    f3 = lambda x: np.cos(x)
+    g3 = lambda x: x+9
     plt_space = np.linspace(-10, 10, 1000)
-    # plt.grid()
-    # plt.plot(plt_space, f1(plt_space))
-    # plt.plot(plt_space, g1(plt_space))
-    # plt.show()
     plotIntersection(plt_space, f1, g1)
     plotIntersection(plt_space, f2, g2)
+    plotIntersection(plt_space, f3, g3)
+    plotIntersection(np.linspace(-8, 4, 1000), f3, f2)
+
