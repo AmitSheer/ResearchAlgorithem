e12d08085a5c97107de659fd5192be3389a3d6dd
Amit-Sheer Cohen <amitcohen95@gmail.com>
2022-05-11 11:34:15 +0300

    finished q2

--- assignment_6/question2.py
+++ assignment_6/question2.py
@@ -1,23 +1,37 @@
 import pandas as pd
 
 pd.set_option('display.max_columns', None)
-global df
-df = pd.read_csv("national-budget.csv")
+
+
+# global df
+# df = pd.read_csv("national-budget.csv")
+
+
+def by_year(year: int):
+    df = pd.read_csv("national-budget.csv", index_col=['שנה'])
+    return df.loc[year]["הוצאה נטו"].sum()
+
+
+def by_year_and_office(year: int, office: str, df = None):
+    if df is None:
+        df = pd.read_csv("national-budget.csv", index_col=['שנה', "שם רמה 2"])
+    return df.loc[year, office]["הוצאה נטו"].sum()
 
 
 def education_budget(year: int) -> int:
-    global df
-    return df[(df["שם רמה 2"] == 'חינוך') & (df['שנה'] == year)]["הוצאה נטו"].sum()
+    return by_year_and_office(year, 'חינוך')
+    # return df[(df["שם רמה 2"] == 'חינוך') & (df['שנה'] == year)]["הוצאה נטו"].sum()
 
 
 def security_budget_ratio(year: int) -> float:
-    global df
-    return df[(df["שם רמה 2"] == 'בטחון') & (df['שנה'] == year)]["הוצאה נטו"].sum() / df[(df['שנה'] == year)][
-        "הוצאה נטו"].sum()
+    return by_year_and_office(year, 'בטחון') / by_year(year)
+    # global df
+    # return df[(df["שם רמה 2"] == 'בטחון') & (df['שנה'] == year)]["הוצאה נטו"].sum() / df[(df['שנה'] == year)][
+    #     "הוצאה נטו"].sum()
 
 
 def largest_budget_year(office: str) -> int:
-    global df
+    df = pd.read_csv("national-budget.csv")
     max_sum = 0.
     max_year = 0.
     for year in df['שנה'].unique():
@@ -30,12 +44,13 @@ def largest_budget_year(office: str) -> int:
 
 # how much did a given department spend abroad over years
 def spending_abroad(office: str) -> int:
-    global df
-    return df[(df["שם רמה 2"] == office) & (df["שם מיון רמה 2"] == 'קניות בחו"ל')]['הוצאה נטו'].sum()
+    df = pd.read_csv("national-budget.csv", index_col=["שם מיון רמה 2", "שם רמה 2"])
+    return df.loc['קניות בחו"ל', office]['הוצאה נטו'].sum()
+    # return df[(df["שם רמה 2"] == office) & (df["שם מיון רמה 2"] == 'קניות בחו"ל')]['הוצאה נטו'].sum()
 
 
 if __name__ == '__main__':
-    print(f'education budget in 2000: {education_budget(2000)}\n --------------------------------------')
+    print(f'education budget in 2000: {education_budget(2000)}₪\n --------------------------------------')
     print(f'security budget ratio in 2000: {security_budget_ratio(2000)}\n --------------------------------------')
     print(f'security largest budget year: {largest_budget_year("בטחון")}\n --------------------------------------')
-    print(f'security spending abroad: {spending_abroad("בטחון")}\n --------------------------------------')
+    print(f'security spending abroad: {spending_abroad("בטחון")}₪\n --------------------------------------')
